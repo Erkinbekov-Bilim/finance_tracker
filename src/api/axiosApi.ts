dc471c9edd9978e7a20779c543bc400f183b2eec
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-07 15:28:59 +0600

    change baseUrl

--- src/api/axiosApi.ts
+++ src/api/axiosApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosApi = axios.create({
   baseURL:
-    'https://js-30-bilim-default-rtdb.europe-west1.firebasedatabase.app/',
+    'https://js-30-bilim-default-rtdb.europe-west1.firebasedatabase.app/finance',
 });
 
 export default axiosApi;
