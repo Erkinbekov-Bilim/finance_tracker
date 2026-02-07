fa14e3c8e5a4a694c7e50c9a95819bcedec470da
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-07 18:46:33 +0600

    create CategoryForm component with react-hook-form

---
+++ src/components/CategoryForm/CategoryForm.tsx
@@ -0,0 +1,143 @@
+import { useEffect, type FC } from 'react';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks/reduxHooks';
+import { selectFinanceTypes } from '../../redux/features/categories/categories.selectors';
+import { getFinanceType } from '../../redux/features/categories/categories.api';
+import type { ICategoriesMutation } from '../../types/finance/categories/categories-mutation';
+import { useForm } from 'react-hook-form';
+import './CategoryForm.css';
+import Loader from '../../UI/Loader/Loader';
+import Button from '../../UI/Button/Button';
+
+interface ICategoryFormProps {
+  onSubmitCategory: (category: ICategoriesMutation) => void;
+  defaultValueCategory?: ICategoriesMutation;
+  isEdit?: boolean;
+  isLoading?: boolean;
+  targetModal?: () => void;
+}
+
+const CategoryForm: FC<ICategoryFormProps> = ({
+  onSubmitCategory,
+  defaultValueCategory,
+  isEdit,
+  isLoading,
+  targetModal,
+}) => {
+  const dispatch = useAppDispatch();
+  const financeTypes = useAppSelector(selectFinanceTypes);
+
+  useEffect(() => {
+    dispatch(getFinanceType());
+  }, [dispatch]);
+
+  const defaultValues: ICategoriesMutation = defaultValueCategory
+    ? defaultValueCategory
+    : {
+        name: '',
+        financeType: '',
+      };
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: defaultValues,
+  });
+
+  const onSubmit = (data: ICategoriesMutation) => {
+    onSubmitCategory(data);
+    targetModal && targetModal();
+    reset();
+  };
+
+  useEffect(() => {
+    if (defaultValueCategory) {
+      reset(defaultValueCategory);
+    }
+  }, [defaultValues, reset]);
+
+  return (
+    <>
+      <div className="form-block">
+        <p className="form-block-title">
+          {isEdit ? 'update category' : 'add category'}
+        </p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-block-content">
+            <div className="form-input-block">
+              <label htmlFor="finance-type">Type</label>
+              <select
+                {...register('financeType', {
+                  required: 'Finance type is required',
+                  disabled: isLoading,
+                })}
+                name="financeType"
+                id="finance-type"
+                title="finance type"
+                className="form-select"
+              >
+                <option disabled className="form-select-option" value="">
+                  Select meal time
+                </option>
+                {financeTypes.map((type) => (
+                  <option
+                    value={type.id}
+                    className="form-select-option"
+                    key={type.id}
+                  >
+                    {type.title}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div className="form-input-block">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                title="Name"
+                id="name"
+                placeholder="Name"
+                className="form-input"
+                {...register('name', {
+                  required: 'Name is required',
+                  disabled: isLoading,
+                  minLength: {
+                    value: 2,
+                    message: 'Name must be at least 2 characters',
+                  },
+                  maxLength: {
+                    value: 40,
+                    message: 'Name must be at most 20 characters',
+                  },
+                  validate: (value) =>
+                    value.trim() !== '' || 'Name is required',
+                })}
+              />
+              {errors.name && (
+                <p className="input-error-message">{errors.name.message}</p>
+              )}
+            </div>
+
+            <Button
+              className="form-submit-btn"
+              type="submit"
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <div className="loader-button">
+                  <Loader />
+                </div>
+              ) : (
+                'save'
+              )}
+            </Button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default CategoryForm;
