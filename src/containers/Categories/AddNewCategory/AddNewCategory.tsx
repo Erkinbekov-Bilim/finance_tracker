035fa3dfe51a67d3b7d17a7370fdbb6ab3e4ea40
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-08 21:57:25 +0600

    add AddNewCategory component with form submission and loading state

---
+++ src/containers/Categories/AddNewCategory/AddNewCategory.tsx
@@ -0,0 +1,31 @@
+import { postFinanceCategory } from '../../../redux/features/categories/categories.api';
+import type { ICategoriesMutation } from '../../../types/finance/categories/categories-mutation';
+import CategoryForm from '../../../components/CategoryForm/CategoryForm';
+import {
+  useAppDispatch,
+  useAppSelector,
+} from '../../../redux/hooks/reduxHooks';
+import { selectLoading } from '../../../redux/features/categories/categories.selectors';
+import type { FC } from 'react';
+
+interface IAddNewCategory {
+  toggleModal: () => void;
+}
+
+const AddNewCategory: FC<IAddNewCategory> = ({ toggleModal }) => {
+  const dispatch = useAppDispatch();
+  const { sendLoading } = useAppSelector(selectLoading);
+
+  const onSubmitCategory = async (category: ICategoriesMutation) => {
+    await dispatch(postFinanceCategory(category));
+    if (toggleModal) {
+      toggleModal();
+    }
+  };
+
+  return (
+    <CategoryForm onSubmitCategory={onSubmitCategory} isLoading={sendLoading} />
+  );
+};
+
+export default AddNewCategory;
