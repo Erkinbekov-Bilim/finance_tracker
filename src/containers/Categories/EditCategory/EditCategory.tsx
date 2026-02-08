5c4c823d95eb855af6e40015dc31ad4364499e24
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-08 21:59:24 +0600

    add EditCategory component with fetch and update loading

---
+++ src/containers/Categories/EditCategory/EditCategory.tsx
@@ -0,0 +1,52 @@
+import { useEffect, type FC } from 'react';
+import {
+  useAppDispatch,
+  useAppSelector,
+} from '../../../redux/hooks/reduxHooks';
+import CategoryForm from '../../../components/CategoryForm/CategoryForm';
+import {
+  getFinanceCategory,
+  putFinanceCategory,
+} from '../../../redux/features/categories/categories.api';
+import {
+  selectFinanceCategory,
+  selectLoading,
+} from '../../../redux/features/categories/categories.selectors';
+import type { ICategoriesMutation } from '../../../types/finance/categories/categories-mutation';
+
+interface IEditCategoryProps {
+  id: string;
+  toggleModal?: () => void;
+}
+
+const EditCategory: FC<IEditCategoryProps> = ({ id, toggleModal }) => {
+  const dispatch = useAppDispatch();
+  const { sendLoading } = useAppSelector(selectLoading);
+  const category = useAppSelector(selectFinanceCategory);
+
+  useEffect(() => {
+    dispatch(getFinanceCategory({ id }));
+  }, [dispatch, id]);
+
+  const onSubmitCategory = async (category: ICategoriesMutation) => {
+    await dispatch(putFinanceCategory({ id, category }));
+    if (toggleModal) {
+      toggleModal();
+    }
+  };
+
+  return (
+    <>
+      {category && (
+        <CategoryForm
+          defaultValueCategory={category}
+          onSubmitCategory={onSubmitCategory}
+          isEdit={true}
+          isLoading={sendLoading}
+        />
+      )}
+    </>
+  );
+};
+
+export default EditCategory;
