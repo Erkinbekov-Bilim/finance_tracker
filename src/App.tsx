af6b69f535aab1769053c5668aeeb28f13b78566
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-08 22:37:17 +0600

    add routes for Categories within MainLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-
+import { Route, Routes } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout/MainLayout';
+import Transactions from './containers/Transactions/Transactions';
+import Categories from './containers/Categories/Categories';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Transactions />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </MainLayout>
+    </>
   );
 };
 
