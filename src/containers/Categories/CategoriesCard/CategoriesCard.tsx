36e978c627ee40f01199db43e8ed69d56141ea1a
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-08 22:14:47 +0600

    add CategoriesCard component with edit and delete func

---
+++ src/containers/Categories/CategoriesCard/CategoriesCard.tsx
@@ -0,0 +1,72 @@
+import { useState, type FC } from 'react';
+import type { ICategories } from '../../../types/finance/categories/categories';
+import './CategoriesCard.css';
+import Button from '../../../UI/Button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faPen } from '@fortawesome/free-solid-svg-icons';
+import { useAppDispatch } from '../../../redux/hooks/reduxHooks';
+import {
+  deleteFinanceCategory,
+  getFinanceCategories,
+} from '../../../redux/features/categories/categories.api';
+import EditCategory from '../EditCategory/EditCategory';
+import Backdrop from '../../../UI/Backdrop/Backdrop';
+import Modal from '../../../UI/Modal/Modal';
+
+interface ICategoriesCardProps {
+  category: ICategories;
+}
+
+const CategoriesCard: FC<ICategoriesCardProps> = ({ category }) => {
+  const dispatch = useAppDispatch();
+  const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+  const toggleModal = () => setIsOpenModal((prev) => !prev);
+
+  const deleteCategory = async (id: string) => {
+    const isConfirmed = confirm(
+      'Are you sure you want to delete this category?',
+    );
+
+    if (isConfirmed) {
+      await dispatch(deleteFinanceCategory({ id })).unwrap();
+      await dispatch(getFinanceCategories());
+    }
+  };
+
+  return (
+    <>
+      {isOpenModal && (
+        <>
+          <Backdrop onClose={toggleModal} />
+          <Modal onClose={toggleModal}>
+            <EditCategory id={category.id} toggleModal={toggleModal} />
+          </Modal>
+        </>
+      )}
+      <div className="category-card">
+        <p className="category-name">{category.name}</p>
+        <div className="category-content">
+          <p className={`category-type category-type-${category.financeType}`}>
+            {category.financeType}
+          </p>
+          <div className="category-actions">
+            <Button
+              className="category-action category-action-delete"
+              onClick={() => deleteCategory(category.id)}
+            >
+              <FontAwesomeIcon icon={faTrash} />
+            </Button>
+            <Button
+              className="category-action category-action-edit"
+              onClick={() => setIsOpenModal(true)}
+            >
+              <FontAwesomeIcon icon={faPen} />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CategoriesCard;
