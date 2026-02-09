4b1f79f123cd5ee6ec4657adb3148aa4699256bf
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-09 23:03:10 +0600

    add edit transaction route

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import MainLayout from './layouts/MainLayout/MainLayout';
 import Transactions from './containers/Transactions/Transactions';
 import Categories from './containers/Categories/Categories';
 import AddNewTransaction from './containers/Transactions/AddNewTransaction/AddNewTransaction';
+import EditTransaction from './containers/Transactions/EditTransaction/EditTransaction';
 
 const App = () => {
   return (
     <>
       <MainLayout>
         <Routes>
-          <Route path="/" element={<Transactions />} />
+          <Route path="/" element={<Transactions />}>
+            <Route path="/transactions/:id/edit" element={<EditTransaction />} />
+          </Route>
           <Route path="/transactions/add" element={<AddNewTransaction />} />
 
           <Route path="/categories" element={<Categories />} />
