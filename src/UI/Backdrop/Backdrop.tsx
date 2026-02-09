92fc40d7e752ae7e461a0f35ba53d36e10fedfe3
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-09 18:01:45 +0600

    add link-based closing support to Modal and Backdrop

--- src/UI/Backdrop/Backdrop.tsx
+++ src/UI/Backdrop/Backdrop.tsx
@@ -1,12 +1,21 @@
 import type React from 'react';
 import './Backdrop.css';
+import { Link } from 'react-router-dom';
+import type { ReactElement } from 'react';
 
 interface IBackdropProps {
-  onClose: () => void;
+  onClose?: () => void;
+  to?: string;
 }
 
-const Backdrop: React.FC<IBackdropProps> = ({onClose}) => {
-  return <div className="backdrop" onClick={onClose}></div>;
+const Backdrop: React.FC<IBackdropProps> = ({ onClose, to = null }) => {
+  const content: ReactElement = <div className="backdrop" onClick={onClose}></div>;
+
+  return to ? (
+    <Link className="backdrop" onClick={onClose} to={to}></Link>
+  ) : (
+    content
+  );
 };
 
 export default Backdrop;
--- src/UI/Modal/Modal.tsx
+++ src/UI/Modal/Modal.tsx
@@ -1,20 +1,35 @@
 import './Modal.css';
-import type { PropsWithChildren } from 'react';
+import type { PropsWithChildren, ReactElement } from 'react';
 import type React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 interface IModal extends PropsWithChildren {
-  onClose: () => void;
+  onClose?: () => void;
+  to?: string;
 }
 
-const Modal: React.FC<IModal> = ({ onClose, children }) => {
+const Modal: React.FC<IModal> = ({ onClose, to, children }) => {
+  const renderContent = (icon: ReactElement) => {
+    if (to)
+      return (
+        <Link className="modal-close-button" to={to}>
+          {icon}
+        </Link>
+      );
+
+    return (
+      <div className="modal-close-button" onClick={onClose}>
+        {icon}
+      </div>
+    );
+  };
+
   return (
     <>
       <div className="modal">
-        <div className="modal-close-button" onClick={onClose}>
-          <FontAwesomeIcon icon={faXmark} />
-        </div>
+        {renderContent(<FontAwesomeIcon icon={faXmark} />)}
         <div className="modal-content">{children}</div>
       </div>
     </>
