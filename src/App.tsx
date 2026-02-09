c8b3f9a770d95e39476378a973673351b8235ce8
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-09 18:25:44 +0600

    add route for creating transaction

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout/MainLayout';
 import Transactions from './containers/Transactions/Transactions';
 import Categories from './containers/Categories/Categories';
+import AddNewTransaction from './containers/Transactions/AddNewTransaction/AddNewTransaction';
 
 const App = () => {
   return (
@@ -9,6 +10,8 @@ const App = () => {
       <MainLayout>
         <Routes>
           <Route path="/" element={<Transactions />} />
+          <Route path="/transactions/add" element={<AddNewTransaction />} />
+
           <Route path="/categories" element={<Categories />} />
         </Routes>
       </MainLayout>
