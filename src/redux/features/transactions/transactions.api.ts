d363503e1334f7047ba2d9eafddc5bc0a91b5a35
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-09 19:47:51 +0600

    add getTransactions think to fetch transactions list from API

--- src/redux/features/transactions/transactions.api.ts
+++ src/redux/features/transactions/transactions.api.ts
@@ -4,6 +4,8 @@ import type { ITransactionCategory } from '../../../types/finance/transactions/t
 import type { ICategoriesApi } from '../../../types/finance/categories/categories-api';
 import type { ITransactionMutation } from '../../../types/finance/transactions/transactiion-mutation';
 import type { ITransactionData } from '../../../types/finance/transactions/transaction-data';
+import type { ITransaction } from '../../../types/finance/transactions/transaction';
+import type { ITransactionApi } from '../../../types/finance/transactions/transaction-api';
 
 export const getTransactionCategory = createAsyncThunk<
   ITransactionCategory[],
@@ -28,6 +30,28 @@ export const getTransactionCategory = createAsyncThunk<
 
   return [];
 });
+
+export const getTransactions = createAsyncThunk<ITransaction[]>(
+  'finance/getTransactions',
+  async () => {
+    const response = await axiosApi.get<ITransactionApi>('/transactions.json');
+    const data = response.data;
+
+    if (data) {
+      const transactionsIDS: string[] = Object.keys(data);
+      const transaction: ITransaction[] = transactionsIDS.map((id) => {
+        return {
+          ...data[id],
+          id,
+        };
+      });
+
+      return transaction;
+    }
+
+    return [];
+  },
+);
 
 export const postTransaction = createAsyncThunk<void, ITransactionMutation>(
   'finance/postTransaction',
